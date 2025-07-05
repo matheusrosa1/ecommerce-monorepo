7a9967d6449447f62894528646a513f9122f1d00
Matheus Rosa <matheusrosataxa@gmail.com>
2025-07-05 16:08:33 -0300

    fix: app.module.ts

--- apps/product-catalog-service/src/app.module.ts
+++ apps/product-catalog-service/src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
+import { Category } from './categories/entities/category.entity';
+import { Product } from './products/entities/product.entity';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Set to false in production
+      entities: [Category, Product],
+      synchronize: true,
     }),
     ProductsModule,
     CategoriesModule,
