3d4d2fc762d3284128553740fb18676ea1d9de16
Matheus Rosa <matheusrosataxa@gmail.com>
2025-07-16 21:56:31 -0300

    feat: ajustes cartService buscando api product catalog

--- apps/shopping-cart-service/src/cart/cart.service.ts
+++ apps/shopping-cart-service/src/cart/cart.service.ts
@@ -1,19 +1,62 @@
+import { HttpService } from '@nestjs/axios';
 import { Inject, Injectable } from '@nestjs/common';
 import Redis from 'ioredis';
+import { firstValueFrom } from 'rxjs';
+import { Product } from 'src/interfaces/Product';
 import { REDIS_CLIENT } from 'src/redis/redis.module';
 import { Cart } from '../interfaces/Cart';
 import { AddItemDto } from './dto/add-item.dto';
 
 @Injectable()
 export class CartService {
+  private readonly catalogServiceUrl =
+    'http://product-catalog-service:3000/products/batch';
   constructor(
     @Inject(REDIS_CLIENT)
     private readonly redisClient: Redis,
+    private readonly httpService: HttpService,
   ) {}
 
   // Chave para armazenar o carrinho no Redis. Ex: "cart:user123"
   private getCartKey(sessionId: string): string {
     return `cart:${sessionId}`;
+  }
+
+  private async enrichCart(cart: Cart): Promise<Cart> {
+    if (!cart || cart.items.length === 0) {
+      return { items: [], total: 0 };
+    }
+
+    const productId = cart.items.map((item) => item.productId).join(',');
+
+    try {
+      const response = await firstValueFrom(
+        this.httpService.get<Product[]>(
+          `${this.catalogServiceUrl}?ids=${productId}`,
+        ),
+      );
+
+      const products = response.data;
+
+      let total = 0;
+
+      const enrichedItems = cart.items.map((item) => {
+        const productDetail = products.find((p) => p.id === item.productId);
+        if (productDetail) {
+          total += productDetail.price * item.quantity;
+          return {
+            ...item,
+            name: productDetail.name,
+            price: productDetail.price,
+          };
+        }
+        return item;
+      });
+      return { items: enrichedItems, total: parseFloat(total.toFixed(2)) };
+    } catch (error) {
+      console.error('Erro ao buscar detalhes dos produtos: ', error);
+      return cart;
+    }
   }
 
   async addItem(sessionId: string, addItemDto: AddItemDto): Promise<Cart> {
@@ -54,7 +97,7 @@ export class CartService {
     // Salva o carrinho atualizado no Redis]
     // Usamos 'EX 86400' para expirar o carrinho em 24 horas
     await this.redisClient.set(cartKey, JSON.stringify(cart), 'EX', 86400); // Expira em 24 horas
-    return cart;
+    return this.enrichCart(cart);
   }
 
   async getCart(sessionId: string): Promise<Cart | null> {
@@ -64,7 +107,8 @@ export class CartService {
     if (!cartData) {
       return null; // Retorna null se o carrinho não existir
     }
-    return JSON.parse(cartData) as Cart;
+    const cart = JSON.parse(cartData) as Cart;
+    return this.enrichCart(cart);
   }
 
   async clearCart(sessionId: string): Promise<void> {
